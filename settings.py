bffc407ae1869e7a3d3bb03e194186cb509d8a65
oleg.gusev <gusevoleg@gmail.com>
2024-07-23 17:50:33 +0300

    init

---
+++ settings.py
@@ -0,0 +1,13 @@
+domain = "https://api.glassen-it.com/"
+auth_url = f"{domain}component/socparser/authorization/login"
+post_url = f"{domain}component/socparser/content/posts"
+
+login = "java_api"
+password = "4yEcwVnjEH7D"
+thread_id = 22183
+referenceFilter = []
+bot_data = "7207416346:AAGnG8MQHNNthTjkBm3DNMNHRDQ6-11mavM"
+chart_id ="-4282064476"
+UTC = 3
+FROM_MINUTES = 25
+POSTS_LIMIT = 5_000
